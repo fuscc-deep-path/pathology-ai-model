7a7e27483a1c36dfb7f85b9ad42bd582adeef88a
YJC <yjcyxky@163.com>
2021-09-28 11:35:46 +0800

    [Fix Bug] incorrect import clause.

--- pathology_ai_model/cli.py
+++ pathology_ai_model/cli.py
@@ -21,10 +21,8 @@ sys.path.insert(0, root_dir)
 import click
 from pathology_ai_model.tumor_detector import start_model as start_detector_model
 from pathology_ai_model.sampling import start_sampling
-from pathology_ai_model.prediction import start_model as start_prediction_model
 from pathology_ai_model.slide2patch import run_slide2patch as start_slide2patch
 from pathology_ai_model.stain_norm import start_stain_norm
-from pathology_ai_model.heatmap import make_heatmap as start_heatmap
 
 @click.group()
 def cli():
@@ -76,33 +74,6 @@ def slide2patch(xmlpath, output):
 def normalization(datapath, output):
     """To normalize the image patches."""
     start_stain_norm(datapath, output)
-
-
-@cli.command()
-@click.option('--datapath', '-d', required=True,
-              type=click.Path(exists=True, dir_okay=True),
-              help="The directory which saved normalized image patches.")
-@click.option('--feats-file', '-f', required=True,
-              help="The file which saved feats (npz file).",
-              type=click.Path(exists=True, file_okay=True))
-@click.option('--sampling-file', '-s', required=True,
-              type=click.Path(exists=True, file_okay=True),
-              help="The file which saved sampling images.")
-@click.option('--root-dir', '-r', required=True,
-              type=click.Path(exists=True, dir_okay=True),
-              help="The root directory which to save result files.")
-@click.option('--model-type', '-m', required=False, 
-              help="The model type for prediction (default: PIK3CA_Mutation).",
-              default='PIK3CA_Mutation', type=click.Choice(['PIK3CA_Mutation', 'BLIS', 'IM', 'LAR',  'MES']))
-@click.option('--gpu', '-g', required=False,
-              help="Which gpu(s) (default: '0')?", default='0')
-@click.option('--num-classes', '-n', required=False,
-              help="How many classes (default: 2)?", default=2, type=int)
-def heatmap(datapath, feats_file, sampling_file, root_dir, model_type, gpu, num_classes):
-    """To make a heatmap for the selected image patches."""
-    start_heatmap(datapath, featsfile=feats_file, sampling_file=sampling_file, root_dir=root_dir, model_type=model_type, gpu=gpu, num_classes=num_classes)
-
-
 
 main = click.CommandCollection(sources=[cli])
 
